e6f58c5595487968774b2c9c123bdf81817b1d8e
BomberBeetle <gdreks@gmail.com>
2023-06-06 11:29:27 -0300

    init.sql

---
+++ init.sql
@@ -0,0 +1,6 @@
+insert into estacionamento values (1,50,'Javalet Indaiatuba', 5, 50,100,300);
+insert into estaciomento values (2,25,'Javalet Salto', 5, 25, 10, 125, 200);
+
+insert into usuario values (1, "111.111.111-11", "porteiro@javalet.com", "Pour Teiro", "senha", 'PORTEIRO');
+insert into usuario values (2, "222.222.222-22", "diretor@javalet.com", "Diré Tour", "senha", 'DIRETOR');
+insert into usuario values (3, "333.333.333-22", "cliente@email.com", "Clee Entea", "senha", 'CLIENTE');
